cec15b5a5e023a885274928a1261309f76ae0305
Gordon Novak <gordie.novak@icloud.com>
2025-12-24 22:55:13 -0600

    Added initializer for InputRouters to prevent out-of-engine user-defined initialization

---
+++ include/engine/input/InputRouterInit.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "engine/EngineContext.hpp"
+
+namespace geng {
+
+    /** Only allows the engine to make an InputRouter **/
+    class InputRouterInit {
+        explicit InputRouterInit(EngineContext& core, uint8_t*& keystates) : core(core), key_states(keystates) {}
+        friend class Engine;
+        friend class InputRouter;
+        EngineContext& core;
+        uint8_t* key_states;
+    };
+}
