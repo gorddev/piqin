6aea682aee66f15320e2da2735216f295b02b5d9
Gordon Novak <gordie.novak@icloud.com>
2025-12-24 22:54:34 -0600

    added default syntaxMap and coloring system

---
+++ include/engine/defaults/GengColors.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <SDL_pixels.h>
+
+namespace geng {
+
+    // primary colors
+    constexpr SDL_Color color_red = {255, 60, 60, 255};
+    constexpr SDL_Color color_orange = {255, 165, 0, 255};
+    constexpr SDL_Color color_yellow = {255, 255, 60, 255};
+    constexpr SDL_Color color_green = {100, 200, 100, 255};
+    constexpr SDL_Color color_blue = {140, 140, 255, 255};
+    constexpr SDL_Color color_purple = {255, 140, 255, 255};
+    // extra colors
+    constexpr SDL_Color color_pink = {255, 170, 170, 255};
+    constexpr SDL_Color color_white = {255, 255, 255, 255};
+    constexpr SDL_Color color_black = {20, 20, 20, 255};
+
+}
